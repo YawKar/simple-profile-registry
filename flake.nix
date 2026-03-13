2df2e7f1bf64fceea0a9dd59b0fc57c3ce63e3de
Side Effect <yawkarpov@gmail.com>
2026-03-13 18:13:53 +0000

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "nixified-typescript-template";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              # Toolchain
+              typescript
+              nodejs_25
+
+              # Automation
+              go-task
+              pre-commit
+
+              # Formatters & Linters
+              yamlfmt
+              nixfmt
+            ];
+
+            shellHook = ''
+              pre-commit uninstall && pre-commit install
+              echo "🚀 Modern TypeScript environment"
+              task
+            '';
+          };
+        };
+    };
+}
