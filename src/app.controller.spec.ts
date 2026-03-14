b1e71192d3547c919134aeb994f57a1334b2e970
yawkar <yawkarpov@gmail.com>
2026-03-14 16:43:49 +0300

    add catscontroller

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
+import { AppController, CatsController } from './app.controller';
 import { AppService } from './app.service';
 
 describe('AppController', () => {
@@ -17,6 +17,24 @@ describe('AppController', () => {
   describe('root', () => {
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
+    });
+  });
+});
+
+describe('CatsController', () => {
+  let catsController: CatsController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [CatsController],
+    }).compile();
+
+    catsController = app.get<CatsController>(CatsController);
+  });
+
+  describe('cats', () => {
+    it('should return all cats', () => {
+      expect(catsController.findAll()).toBe('This method returns all cats');
     });
   });
 });
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -10,3 +10,11 @@ export class AppController {
     return this.appService.getHello();
   }
 }
+
+@Controller('cats')
+export class CatsController {
+  @Get()
+  findAll(): string {
+    return 'This method returns all cats';
+  }
+}
